8249624b983f968276d5a4fcee232f6b26d66887
Jacob Ulrich <julrich@gmail.com>
2017-11-02 11:32:58 -0700

    Initial Commit

---
+++ Seattle Streetcar TrackerTests/Seattle_Streetcar_TrackerTests.swift
@@ -0,0 +1,36 @@
+//
+//  Seattle_Streetcar_TrackerTests.swift
+//  Seattle Streetcar TrackerTests
+//
+//  Created by Jacob Ulrich on 11/2/17.
+//  Copyright © 2017 Jacob Ulrich. All rights reserved.
+//
+
+import XCTest
+@testable import Seattle_Streetcar_Tracker
+
+class Seattle_Streetcar_TrackerTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
